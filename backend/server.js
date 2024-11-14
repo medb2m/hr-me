03c7527df1900ba63f5b5364d11f0894f571c31e
medb2m <79202818+medb2m@users.noreply.github.com>
2024-11-14 03:59:24 +0100

    added morgan & nodemon dev-dependencies and bodyParser + cookieParser | backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
 import dotenv from 'dotenv';
+
+// Routes
 import candidateRoutes from './routes/candidate.routes.js';
 
 dotenv.config();
@@ -10,8 +15,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(morgan("dev"))
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI).then(() => console.log('Connected to MongoDB'))
